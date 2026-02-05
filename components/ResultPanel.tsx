0916be828644aea1445683a11539987f1fee50e0
Akshay Mirchandani <akshaymir@gmail.com>
2026-02-05 07:52:57 +0530

    Result panel: download + click-to-zoom + hover zoom

--- components/ResultPanel.tsx
+++ components/ResultPanel.tsx
@@ -16,16 +16,42 @@ export default function ResultPanel({
   emptyText = 'No result yet',
 }: ResultPanelProps) {
   const [active, setActive] = useState<string | undefined>(undefined);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     if (images.length > 0) setActive(images[0]);
     else setActive(undefined);
   }, [images]);
 
+  // ESC to close modal
+  useEffect(() => {
+    function onKey(e: KeyboardEvent) {
+      if (e.key === 'Escape') setOpen(false);
+    }
+    window.addEventListener('keydown', onKey);
+    return () => window.removeEventListener('keydown', onKey);
+  }, []);
+
   return (
     <div className="panel rp">
       <div className="panelHeader rp_head">
         <div className="rp_title">{title}</div>
+
+        {/* Right-side actions */}
+        <div className="rp_actions">
+          {active ? (
+            <a
+              className="rp_download"
+              href= "_blank"
+              rel="noreferrer"
+              title="Download (or open in new tab)"
+            >
+              Download
+            </a >
+          ) : (
+            <span className="rp_download rp_download--disabled">Download</span>
+          )}
+        </div>
       </div>
 
       <div className="panelBody rp_body">
@@ -45,11 +71,18 @@ export default function ResultPanel({
 
         {!loading && images.length > 0 && active && (
           <>
-            <div className="rp_stage rp_stage--grey rp_fit--contain">
+            {/* Stage */}
+            <button
+              type="button"
+              className="rp_stage rp_stage--grey rp_fit--contain"
+              onClick={() => setOpen(true)}
+              title="Click to view large"
+            >
               {/* eslint-disable-next-line @next/next/no-img-element */}
               < img className="rp_stageImg" src={active} alt="Generated result" />
-            </div>
+            </button>
 
+            {/* Thumbs */}
             <div className="rp_thumbs">
               {images.map((url, i) => (
                 <button
@@ -57,6 +90,7 @@ export default function ResultPanel({
                   type="button"
                   className={`rp_thumbBtn ${url === active ? 'isActive' : ''}`}
                   onClick={() => setActive(url)}
+                  title={`Result ${i + 1}`}
                 >
                   {/* eslint-disable-next-line @next/next/no-img-element */}
                   < img className="rp_thumbImg" src={url} alt={`Result ${i + 1}`} />
@@ -66,6 +100,25 @@ export default function ResultPanel({
           </>
         )}
       </div>
+
+      {/* Modal */}
+      {open && active && (
+        <div className="rp_modal" onClick={() => setOpen(false)} role="presentation">
+          <div className="rp_modalInner" onClick={(e) => e.stopPropagation()}>
+            <div className="rp_modalTop">
+              <a className="rp_download" href={active} download target="_blank" rel="noreferrer">
+                Download
+              </a >
+              <button className="rp_close" type="button" onClick={() => setOpen(false)}>
+                Close
+              </button>
+            </div>
+
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            < img className="rp_full" src={active} alt="Preview" />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
